bc1a55e33a3584a8e4f07aef9051416196920f11
brianasaunders <bnsz9f@mail.missouri.edu>
2017-04-12 21:52:16 -0500

    JavaScript Assignment

---
+++ JavaScript examples w12/js_marquis/js/script.js
@@ -0,0 +1,38 @@
+var newLink = document.createElement ("a");
+
+var allParagraphs = document.getElementsByTagName("p");
+
+var firstParagraph = allParagraphs[0];
+
+function revealCopy(e){
+  for (var i = 0; i < allParagraphs.length; i++ ) {
+    if( i === 0){
+      continue;
+    }
+    allParagraphs[i].style.display = "block";
+  }
+  this.remove()
+  e.preventDefault();
+
+}
+
+newLink.setAttribute("href","#");
+
+newLink.setAttribute ("class", "more-link")
+
+newLink.innerHTML="Read More";
+
+firstParagraph.appendChild( newLink );
+
+//newLink.style.display = "inline-block";
+
+//newLink.style.marginleft = "0.5em";
+
+newLink.addEventListener("click", revealCopy);
+
+for (var i = 0; i < allParagraphs.length; i++ ) {
+  if( i === 0){
+    continue;
+  }
+  allParagraphs[i].style.display = "none";
+}
